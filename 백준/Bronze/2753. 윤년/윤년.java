76cccca7c0a342439be96a1a6c319958454f4c56
rayung999 <108377594+rayung999@users.noreply.github.com>
2023-03-15 16:46:14 +0900

    [Bronze V] Title: 윤년, Time: 124 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int y = Integer.parseInt(br.readLine());
+        
+        System.out.println((y%4==0) ? ((y%400==0) ? "1" : (y%100==0) ? "0" : "1") : "0");
+            
+    }
+}
