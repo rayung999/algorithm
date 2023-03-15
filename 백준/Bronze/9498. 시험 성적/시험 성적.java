343b8486b0ac01519225b368743d5c84ac79f2b0
rayung999 <108377594+rayung999@users.noreply.github.com>
2023-03-15 16:12:29 +0900

    [Bronze V] Title: 시험 성적, Time: 128 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -1,30 +1,18 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       
+        int score = Integer.parseInt(br.readLine());
         
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int score = Integer.parseInt(st.nextToken());
-        
-        if (score >=90) {
-            System.out.println("A");
-        }
-        else if (score >=80) {
-            System.out.println("B");
-        }
-        else if (score >=70) {
-            System.out.println("C");
-        }
-        else if (score >=60) {
-            System.out.println("D");
-        }
-        else {
-            System.out.println("F");
-        }
+        if (score >=90) {System.out.println("A");}
+        else if (score >=80) {System.out.println("B");}
+        else if (score >=70) {System.out.println("C");}
+        else if (score >=60) {System.out.println("D");}
+        else {System.out.println("F"); }
     }
 }
